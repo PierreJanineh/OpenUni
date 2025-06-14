c6d18dee2b0183996f5dae2eb1f90c2f6f706b5d
Pierre Janineh <janinehpierre@gmail.com>
2025-06-14 11:05:00 +0300

    Maman14 - Source questions' Classes

---
+++ src/Maman14/MatrixList.java
@@ -0,0 +1,49 @@
+
+/**
+ * Write a description of class MatrixList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MatrixList
+{
+    IntNodeMat _m00;
+
+    public MatrixList()
+    {
+        _m00 = null;
+    }
+
+    public MatrixList(int[][]mat)
+    {
+
+    }
+
+
+    public int getDataIJ (int i, int j)
+    {
+        return 0;
+    }
+
+    public void setDataIJ (int data, int i, int j)
+    {
+
+    }
+
+
+    public String toString()
+    {
+        return null;
+    }
+
+    public boolean isDescending()
+    {
+        return false;
+    }
+
+    public int howManyNegative()
+    {
+        return 0;
+    }
+
+}
